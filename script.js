20f817d11d054074fb9984b9d07c3f2ce14101ba
RemixN1V <42613527+RemixN1V@users.noreply.github.com>
2021-07-06 17:15:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let style = document.createElement("STYLE");
 style.innerText = `.button {
-color: ${sessionStorage.getItem(color)} !important;
+color: ${sessionStorage.getItem('color')} !important;
 }`;
 document.head.appendChild(style);
 
