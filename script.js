2647494acb9e15704ef1967f15d2ff85ad6a1ee2
RemixN1V <42613527+RemixN1V@users.noreply.github.com>
2021-07-06 17:45:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -75,7 +75,7 @@ function buttonclick(id, name) {
 async function detectColor(forum) {
     if (forum.color !== '' && forum.color !== null) {
         return `background: ${forum.color}`;
-    } else return;
+    } else return '';
 }
 
 let obj = sessionStorage.getItem('obj');
@@ -88,7 +88,7 @@ if (obj.length > 0) {
     splitArray(obj, 3).forEach(forums => {
         forums.forEach(forum => {
             console.log(forum);
-            group.append(`<input style="margin-top: 3px; ${await detectColor(forum)}" type="button" class="button" value="${forum.name}" id="${forum.id}" title="${forum.name}" name="${forum.name}" tabindex="1" onclick="buttonclick(this.id, this.name)"> `);
+            group.append(`<input style="margin-top: 3px; ${detectColor(forum).then(x => x)}" type="button" class="button" value="${forum.name}" id="${forum.id}" title="${forum.name}" name="${forum.name}" tabindex="1" onclick="buttonclick(this.id, this.name)"> `);
             if (forums.indexOf(forum) == 2) group.append("<br>");
         });
     });
